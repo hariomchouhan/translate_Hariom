21f5b8cca4dfcc76be787a3c3d54cf92774c355d
Hariom Chouhan <75517242+hariomchouhan@users.noreply.github.com>
2024-05-08 14:12:06 +0000

    add same remaining files

---
+++ src/redux/actions/index.js
@@ -0,0 +1,58 @@
+export const inputLang=(inLang={label: "English", value: "en"})=>{
+    return{
+        type: "INPUT_LANGUAGE",
+        payload: {inLang:inLang}
+    }
+}
+
+export const detectLang=(detectLang={label: "English", value: "en"})=>{
+    return{
+        type: "DETECT_LANGUAGE",
+        payload: {detectLang: detectLang}
+    }
+}
+
+export const outputLang=(outLang={label: "Hindi", value: "hi"})=>{
+    return{
+        type: "OUTPUT_LANGUAGE",
+        payload: {outLang:outLang}
+    }
+}
+
+export const switchLang=()=>{
+    return{
+        type: "SWITCH",
+    }
+}
+
+export const dropdownActiveIn=()=>{
+    return{
+        type: "ACTIVATE_IN",
+    }
+}
+
+export const dropdownActiveOut=()=>{
+    return{
+        type: "ACTIVATE_OUT",
+    }
+}
+
+export const dropdownDeactive=()=>{
+    return{
+        type: "DEACTIVATE"
+    }
+}
+
+export const inputText=(text)=>{
+    return{
+        type: "INPUT_TEXT",
+        payload: {inputText: text}
+    }
+}
+
+export const outputText=(text)=>{
+    return{
+        type: "OUTPUT_TEXT",
+        payload: {outputText: text}
+    }
+}
